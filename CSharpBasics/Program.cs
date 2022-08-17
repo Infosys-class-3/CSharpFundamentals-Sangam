2dc2141f00d875cad60dc158dfa0a7bde152b9ff
Sangam Ghimire <sangamghimire9@gmail.com>
2022-08-17 08:17:28 +0545

    created consoleapp

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,40 @@
+﻿//Console.WriteLine("Enter number:");//in condition of infinite loop we can use ctrl + c
+//var numberText = Console.ReadLine(); // sometimes red lines appears under text they are called squiggles
+//var number  = int.Parse(numberText);//if we have to convert string into number
+//Datatypes
+//1.Numbers
+//1.1 Integral
+
+byte a = 255; //Its range is 0-255  -It is unsigned and takes sigle byte
+short b = 32767 ;//short is signed and can take two byte and we also can use ushort in case of unsigned short.
+int c = 35234566;
+long d = 3456897213489;
+
+
+//1.2 Floating point numbers, Precision
+
+float e  = 45.6f ;//it can represent up to 6-7 digit after decimal and to approve it as decimal we should add suffix f on end.
+double f = 45.6758944433333;
+decimal g = 46.678954312459m;
+
+//2. Text
+string h = @"aaaaaaaaaaaaaaaa  sf\d";
+char i = 'M';
+//Escape sequences: \n \t \r \b
+Console.WriteLine("I am person. \n I live in earth");
+//@ is called Verbatim character
+
+
+//3.Boolean
+bool hh = true;
+
+//Nullables
+int? population  = null;
+bool? j = null;
+
+
+//Static/strongly typed vs dynamic/weakly typed
+//dynamic language is mostly used in scripting world. 
+
+
+
