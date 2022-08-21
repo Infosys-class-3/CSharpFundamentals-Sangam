039875276fbd3ecdaa71163f8ee0ab7f276c5093
Sangam Ghimire <sangamghimire9@gmail.com>
2022-08-21 06:53:17 +0545

    assignment4

--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -40,4 +40,39 @@ bool? j = null;
 // 2. Declare and intitialize-variables to represent age, name, gender and weight of  a person .Gender of that person should be nullable.*/
 
 
-int age
+//int age
+internal class Employee
+{
+    internal string? name;
+    internal string? email;
+    internal string? contact;
+    internal bool? isMale;
+    internal byte age;
+
+    internal string GetName()
+    {
+        return name;
+    }
+    internal void GetInfo()
+    {
+        System.Console.WriteLine($"Name : {name} \nEmail : {email} \nContact : {contact} \nMale : {isMale} \nAge : {age}"     );
+    }
+}
+
+internal class Program1
+{
+    public static void Main(String[] args)
+    {
+        Employee emp = new();
+        emp.name = "Ram";
+        emp.email = "ram@gmail.com";
+        emp.contact = "980122389";
+        emp.isMale = true;
+        emp.age = 30;
+
+        Console.WriteLine("Employ name: " + emp.GetName());
+        Console.WriteLine("ram");
+        emp.GetInfo();
+    }
+    
+}
